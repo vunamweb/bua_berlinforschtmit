3707007118046318257a37ea0d6c321c798b0f22
vunamweb <vukynamkhtn@gmail.com>
2022-08-15 11:00:37 +0700

    diary
    add, edit comment

--- page/diary.php
+++ page/diary.php
@@ -10,6 +10,7 @@
 /*ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);*/
+include "morpheus/_tinymce.php";
 
 global $arr_form, $table, $tid, $filter, $nameField, $sortField, $imgFolderShort, $ebene, $parent, $morpheus;
 
@@ -32,15 +33,24 @@ $output = '<div id=vorschau>
 ';
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 $arr_form = array(
-    array("name", "Name", '<input type="Text" value="#v#" class="form-control" name="#n#" />'),
+	array("title", "Title", '<input type="Text" value="#v#" class="form-control" name="#n#" />'),
+	array("message", "Message", '<textarea class="summernote form-control" name="#n#" />#v#</textarea>'),
+	array("parent", "", '<input type="hidden" value="#v#" class="form-control" name="#n#" />'),
+	array("uid", "", '<input type="hidden" value="#v#" class="form-control" name="#n#" />'),
+	array("date_time", "", '<input type="hidden" value="#v#" class="form-control" name="#n#" />'),
 );
 ///////////////////////////////////////////////////////////////////////////////////////
 $neuerDatensatz = isset($_GET["neu"]) ? $_GET["neu"] : 0;
 $edit = isset($_REQUEST["edit"]) ? $_REQUEST["edit"] : 0;
 $save = isset($_REQUEST["save"]) ? $_REQUEST["save"] : 0;
+$save_note = isset($_REQUEST["save_note"]) ? $_REQUEST["save_note"] : 0;
 $del = isset($_REQUEST["del"]) ? $_REQUEST["del"] : 0;
 $delete = isset($_REQUEST["delete"]) ? $_REQUEST["delete"] : 0;
 $back = isset($_POST["back"]) ? $_POST["back"] : 0;
+$listHashtag = $_POST['listHashTag'];
+$mediaId = $_REQUEST['mediaId'];
+$parent_comment = $_REQUEST['parent_comment'];
+$message_comment = $_REQUEST['message_comment'];
 
 ///////////////////////////////////////////////////////////////////////////////////////
 function liste()
@@ -75,7 +85,7 @@ function showComments()
 	
 	$echo = '<form method="post" action="' . $url . '"><div class="accordion" id="accordionShowComment">';
 
-    $sql = "SELECT * FROM morp_note WHERE parent = 0 ORDER BY date_time";
+    $sql = "SELECT * FROM morp_note WHERE parent = 0 ORDER BY date_time desc";
 	$res = safe_query($sql);
 	
 	if(mysqli_num_rows($res) == 0)
@@ -101,7 +111,8 @@ function showComments()
 				   data-target="#collapse' . $idNote . '">
 				   <i class="fa fa-plus fa1"></i>
 				   ' . $comment . '
-			   </a>
+				</a>
+				<a class="edit_note" href="?edit='.$idNote.'"><i class="fa fa-pencil-square-o"></i></a>
 		   </h2>
 	   </div>
 	   <div id="collapse' . $idNote . '" class="collapse"
@@ -116,7 +127,7 @@ function showComments()
    </div>';
     }
 
-    $echo .= '</div><input type="hidden" name="save" value="1"><input type="hidden" name="mediaId" value="' . $show_comment . '"><input type="hidden" name="parent_comment" id="parent_comment"><input type="hidden" name="message_comment" id="message_comment"></form>';
+    $echo .= '</div><input type="hidden" name="save_note" value="1"><input type="hidden" name="mediaId" value="0"><input type="hidden" name="parent_comment" id="parent_comment"><input type="hidden" name="message_comment" id="message_comment"></form>';
 
     if ($parent_comment > 0) {
         $echo .= '<script>
@@ -138,7 +149,7 @@ function showCommentOfComment($parent, $level)
 
     $level++;
 
-    $sql = "SELECT * FROM morp_note WHERE parent = " . $parent . " ORDER BY date_time";
+    $sql = "SELECT * FROM morp_note WHERE parent = " . $parent . " ORDER BY date_time desc";
     //echo $sql . '////';
     $res = safe_query($sql);
     //echo mysqli_num_rows($res); die();
@@ -168,6 +179,53 @@ function showCommentOfComment($parent, $level)
 }
 // END
 
+// VU: show hashtag
+function showHashTag($idNote)
+{
+    $response = '';
+    $array_properties = array();
+
+    $query = "SELECT idHashtag FROM morp_note_hashtag where idNote = " . $idNote . "";
+    $result = safe_query($query);
+
+    while ($row = mysqli_fetch_object($result)) {
+        $array_properties[] = $row->idHashtag;
+    }
+
+    $query = "SELECT * FROM morp_hashtag";
+    $result = safe_query($query);
+
+    while ($row = mysqli_fetch_object($result)) {
+        $check = (in_array($row->htID, $array_properties)) ? "checked" : '';
+        $response .= '<div class="show_hashtag"><input ' . $check . ' type="checkbox" value=' . ($row->htID) . ' name="listHashTag[]">&nbsp; &nbsp; &nbsp;<label>' . ($row->name) . '</label></div>';
+    }
+
+    return $response;
+}
+// END
+
+// VU: update hashtag
+function updateHashtagNote($listHashtag, $idNote)
+{
+	// delete all hashtag of this note
+	$sql = 'delete from morp_note_hashtag where idNote = ' . $idNote . '';
+	safe_query($sql);
+	
+	foreach ($listHashtag as $item) {
+        if ($item != '') {
+			// delete if exist
+            $sql = 'delete from morp_note_hashtag where idNote = ' . $idNote . ' and idHashtag = ' . $item . '';
+            //echo $sql; die();
+            safe_query($sql);
+            // insert
+            $sql = 'insert into morp_note_hashtag(idNote, idHashtag)values(' . $idNote . ', ' . $item . ')';
+            //echo $sql . '<br>';
+            safe_query($sql);
+        }
+    }
+}
+// END
+
 function neu() {
 	global $arr_form, $table, $tid, $um_wen_gehts, $ebene, $parent;
 
@@ -176,21 +234,25 @@ function neu() {
 	$echo .= '<a onclick="history.back()"><i class="fa fa-arrow-circle-left"></i> zurück</a><br><br>';
 
     $echo .= '<input type="Hidden" name="neu" value="1"><input type="Hidden" name="save" value="1">
-<input type="hidden" value="'.$ebene.'" class="form-control" name="ebene" readonly="" style="width:120px">
-<input type="hidden" value="'.$parent.'" class="form-control" name="parent" readonly="" style="width:120px">
-
-	<table cellspacing="6" style="width:100%;">';
+    <table cellspacing="6" style="width:100%;">';
 
 	foreach($arr_form as $arr) {
-		if($arr[3]=="GET") $get=$_GET[$arr[0]];
-		if ($x < 3) $echo .= '<tr>
-			<td>'.$arr[1].':</td>
+		$echo .= '<tr>
+			<td>'.$arr[1].'</td>
 			<td>'. str_replace(array("#v#", "#n#", "#s#"), array($get, $arr[0], 'width:400px;'), $arr[2]).'</td>
 		</tr>';
 		$x++;
 	}
 
 	$echo .= '<tr>
+		<td>HashTag</td>
+		<td>
+			'.showHashTag(0).'
+		</td>
+	</tr>
+    ';
+
+  $echo .= '<tr>
 		<td></td>
 		<td>
 			<br>
@@ -199,8 +261,7 @@ function neu() {
 	</tr>
 </table>';
 
-
-	return $echo;
+  return $echo;
 }
 
 function edit($edit)
@@ -227,7 +288,9 @@ function edit($edit)
 
     foreach ($arr_form as $arr) {
         $echo .= setMorpheusForm($row, $arr, $imgFolderShort, $edit, 'morp_referenzen', $tid);
-    }
+	}
+	
+	$echo .= '<p> HashTag: '.showHashTag($edit).'</p><br><br>';
 
     $echo .= '</div>
 		</div>
@@ -248,9 +311,14 @@ function edit($edit)
 if ($save) {
 	$neu = isset($_POST["neu"]) ? $_POST["neu"] : 0;
 
-    $edit = saveMorpheusForm($edit, $neu, 0);
+	$_POST['uid'] = $_SESSION['mid'];
+	$_POST['parent'] = 0;
+	$_POST['date_time'] = date("Y-m-d");
 
-    // if(neu) unset($neu);
+	$edit = saveMorpheusForm($edit, $neu, 0);
+
+    // update hashtag list
+    updateHashtagNote($listHashtag, $edit);
 
     $scriptname = $morpheus['url'] . 'de/' . $_REQUEST['hn'] . '/';
 
@@ -267,7 +335,17 @@ if ($save) {
 	</script>
 <?php
 }
-} elseif ($delete) {
+}
+elseif($save_note) {
+	$uid = $_SESSION['mid'];
+    $date = date('y-m-d');
+
+    $sql = 'insert into morp_note(uid, parent, mediaID, message, date_time)values(' . $uid . ', ' . $parent_comment . ', ' . $mediaId . ', "' . $message_comment . '", "' . $date . '")';
+    safe_query($sql);
+
+    $output = $new . liste();
+} 
+elseif ($delete) {
     $sql = "DELETE FROM `$table` WHERE $tid=$delete ";
 	safe_query($sql);
 	
