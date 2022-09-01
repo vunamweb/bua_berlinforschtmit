ef8beebe5dc318ce05630929197af7d7f5754666
vunamweb <vukynamkhtn@gmail.com>
2022-09-01 17:11:06 +0700

    restore fix modal

--- design/footer_inc.php
+++ design/footer_inc.php
@@ -88,7 +88,7 @@ if($morpheus_edit) include("design/edit.php");
 <script src="<?php echo $dir; ?>js/siriwave/dist/siriwave.umd.js"></script>
 <script type="text/javascript" src="<?php echo $dir; ?>js/siriwave/etc/dat.gui.js"></script>
 <script type="text/javascript" src="<?php echo $dir; ?>js/wavesurfer.js"></script>
-<?php if($_REQUEST['hn'] == 'diary')  ?>
+<?php if($_REQUEST['hn'] == 'diary') {  ?>
 <script type="text/javascript" src="<?php echo $dir; ?>js/semantic.js"></script>
 <?php } ?>
 <script type="text/javascript" src="<?php echo $dir; ?>morpheus/js/selectize.js"></script>
