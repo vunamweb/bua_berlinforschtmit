71ef3fa044a36ecb3884f0b84170ff543c3c87ac
vunam <vukynamkhtn@gmail.com>
2025-06-06 11:07:59 +0700

    integrate laravel

--- bricks/t1__fliesstext_multicolumn.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$output .= '<div class="col-50"><p>'.nl2br($text).'</p></div>';
-$morp = $text;	
-	
--- bricks/t2__texteditor-column.php
+++
@@ -1,4 +0,0 @@
-<?php
-$output .= '<div class="col-50">'.$text.'</div>';
-
-$morp= substr(strip_tags($text),0,200);
